e8b15f50ea3ea5c11219cd0b7db007ebec712759
Alex Varghese <alex.varghese77@gmail.com>
2018-03-19 10:46:48 -0800

    new contactlist application created

---
+++ contactlist/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+import {Header} from './components/header.js'
+
+class App extends Component {
+  render() {
+    return (
+      <Header/>
+    );
+  }
+}
+
+export default App;
---
+++ contactlist/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './../App.css';
+export const Header = () => {
+    return (
+        <div className="header">
+            <h1>Contact</h1>
+            
+        </div>
+
+    )
+} 
