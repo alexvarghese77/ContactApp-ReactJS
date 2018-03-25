f3ed757aa90069edb473a8e13f67589336f10fb0
Alex Varghese <alex.varghese77@gmail.com>
2018-03-25 00:30:43 -0800

    code modified

--- contactlist/src/dev/js/actions/index.js
+++ contactlist/src/dev/js/actions/index.js
@@ -1,5 +1,4 @@
 export const ContactDetail=(Contact)=>{
-    console.log("You are clicked an action")
     return {
         type: 'USER_SELECTED',
         payload: Contact 
--- contactlist/src/dev/js/reducers/reducer-contact-details.js
+++ contactlist/src/dev/js/reducers/reducer-contact-details.js
@@ -1,6 +1,8 @@
 export default (state=null,action)=>{
      switch(action.type){
-         case 'USER_SELECTED': return action.payload;break;
+         case 'USER_SELECTED': 
+                                return action.payload;
+                                break;
      }
      return state;
 }
