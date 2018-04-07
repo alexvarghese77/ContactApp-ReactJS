e4415954f9f64ab78438049357a5c224dcefa21a
Alex Varghese <alex.varghese77@gmail.com>
2018-04-07 12:13:28 -0800

    contact add page added

--- contactlist/src/dev/js/components/AppMain.js
+++ contactlist/src/dev/js/components/AppMain.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import ContactList from '../continers/ContactList'
 import ContactDetails from '../continers/ContactDetails'
+import AddContact from '../continers/AddContact'
+
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 const AppMain=()=>{
@@ -9,6 +11,7 @@ const AppMain=()=>{
             <div>
         <Route path='/' exact component={ContactList}/>
         <Route path='/contactdetail' exact component={ContactDetails}/>
+        <Route path='/AddContact' exact component={AddContact}/>
         </div>
         </Router>
         )
---
+++ contactlist/src/dev/js/continers/AddContact.js
@@ -0,0 +1,42 @@
+import React,{Component} from 'react'
+import { Form, FormGroup, Label, Input, FormFeedback, FormText,Row,Col,Container,Button} from 'reactstrap';
+
+export default class AddContact extends Component{
+    render()
+    {
+        return(
+            <div>
+            <div className="header">
+                    <h1>Add New Contact </h1>
+                    </div>
+          <Row>
+              <Col sm={{ size: 6, offset: 3 }}>
+              <Form>
+        <FormGroup>
+          <Label for="fname">First Name*</Label>
+          <Input type="text" name="fname" id="fname" placeholder="First Name" required/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="mname">Middle Name</Label>
+          <Input type="text" name="mname" id="mname" placeholder="Middle Name" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="lname">Last Name*</Label>
+          <Input type="text" name="lname" id="lname" placeholder="Lirst Name" required/>
+        </FormGroup>
+        <FormGroup>
+          <Label for="contact">Contact Number*</Label>
+          <Input type="number" name="contact" id="contact" placeholder="Contact Number" required />
+        </FormGroup>
+        <FormGroup>
+          <Label for="email">Email Id*</Label>
+          <Input type="email" name="email" id="email" placeholder="Email" required/>
+        </FormGroup>
+        <Button type="submit">Add</Button>
+        </Form>
+              </Col>
+          </Row>
+          </div>
+        )
+    }
+}
